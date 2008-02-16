2e9a6668628a93b68be003f80b0b2e8678cf3e6f
Dr Nic <drnicwilliams@gmail.com>
2008-02-16 15:34:04 +1000

    replying to author with url works

--- src/reply.js
+++ src/reply.js
@@ -1,7 +1,11 @@
 DrNic.CommentReplies.replyToComment = function() {
   var comment = this.parentNode;
   var cite    = comment.getElementsByTagName('cite')[0];
-  var author  = cite.innerHTML;
-  
+  var author_link = cite.getElementsByTagName('a')[0];
+  if (author_link) {
+    var author = author_link.innerHTML;
+  } else {
+    var author = cite.innerHTML;
+  }
   document.getElementById('comment').value = '@' + author + ' - ';
 };
